0c05d085e67f9b37833391ad5b9d62217ba09690
Eunice Pereira <eunicepere333@gmail.com>
2021-07-19 00:13:21 -0500

    file organization

--- interview-prep/funWithAnagram.js
+++ interview-prep/funWithAnagram.js
@@ -1,5 +1,3 @@
-
-
 // Create an object placeholder that will be where we group anagram words
 // loop over text string
 // check if the words in array contain the same letters as the new sorted words
